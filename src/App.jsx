a98773675b45b1b5f3499bb89ac6cf5528f21976
= <=>
2024-04-08 08:18:22 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,6 @@ const { Header, Footer, Sider, Content } = Layout
 const { Title } = Typography
 
 const headerStyle = {
-  textAlign: 'center',
-  color: '#fff',
-  height: 60,
-  paddingInline: 20,
-  lineHeight: '10px',
-  backgroundColor: '#2048bb',
 };
 const contentStyle = {
   textAlign: 'center',
@@ -25,26 +19,31 @@ const siderStyle = {
   textAlign: 'center',
   lineHeight: '120px',
   color: '#fff',
-  backgroundColor: '#bbbbbb',
+  backgroundColor: '#FFFFFF',
+  border: "3px solid black"
 };
 const layoutStyle = {
   borderRadius: 8,
   overflow: 'hidden',
   width: 'calc(100% - 2px)',
   maxWidth: 'calc(100% - 2px)',
-  height: 'calc(100% - 5px)'
+  height: 'calc(100% - 5px)',
 };
 
 let albumInfo = {
   jacketURL: "https://i.discogs.com/HuJE8Ak8vykYnA28p8RcJeYroEnoPuyPfd4p0g7ARDA/rs:fit/g:sm/q:90/h:500/w:500/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTczOTc2/ODQtMTQ0MDYyMTg0/NC04MTg4LmpwZWc.jpeg",
-  titext: "Kalafina - WTF?xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  titletext: "Kalafina - WTF?xxタイトルのてすと感じ漢字xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
   labelText: "SME Records - SEJL-28",
   formatText: "Vinyl, 12 33 RPM",
   countryText: "Japan",
   releasedText: "Aug 12, 2015",
   genreText: "Pop, stage",
   styext: "J-pop",
-  prize: [0, 0, 0],
+  prize: {
+    high: 2000,
+    mid: 1000,
+    low: 500
+  },
   sellsText: "6 copies from ¥11,377",
   haveWant: [10, 20],
   playList: [1, 2, 3, 4, 5, 4, 5, 6, 4, 2, 2, 3, 6, 6, 3, 63, 2]
@@ -57,15 +56,22 @@ function App() {
       <Layout style={layoutStyle}>
         <Sider width="25%" style={siderStyle}>
           <Flex vertical align='start' justify='start'>
-            <UJacket jacketURL={jacketURL} fallBackURL={"public/error-image.png"} />
-            <UPlayList playList={playList} />
+            <UJacket jacketURL={albumInfo.jacketURL} fallBackURL={"public/error-image.png"} />
+            <UPlayList playList={albumInfo.playList} />
           </Flex>
         </Sider>
         <Layout>
-          <Header style={headerStyle}>
-            <UTitle titleText={titleText} />
+          <Header style={{
+            paddingTop: 0,
+            textAlign: 'center',
+            height: 40,
+            lineHeight: '30px',
+            backgroundColor: '#FFFFFF',
+            border: "3px solid black"
+          }}>
+            <UTitle titleText={albumInfo.titletext} />
           </Header>
-          <Content >
+          <Content style={contentStyle}>
             <UAlbumData albumData={albumData} />
           </Content>
         </Layout>
@@ -84,18 +90,14 @@ function UJacket({ jacketURL, fallBackURL, onErrorFunc }) {
 
 function UPlayList({ playList }) {
   return (
-    <ConfigProvider theme={{
-      components: {
-        List: {
-          itemPaddingSM: 4,
-          emptyTextPadding: 2
-        }
-      }
-    }}>
-      <List size="small"
-        bordered dataSource={playList}
-        renderItem={(item) => <List.Item>{item}</List.Item>} />
-    </ConfigProvider>)
+    <List size="small"
+      bordered dataSource={playList}
+      style={{
+        overflow: "auto",
+        height: 400
+      }}
+      renderItem={(item) => <List.Item>{item}</List.Item>} />
+  )
 }
 
 let ellipsisConfig = {
@@ -105,23 +107,15 @@ let ellipsisConfig = {
 
 function UTitle({ titleText }) {
   return (
-    <ConfigProvider theme={{
-      components: {
-        Typography: {
-          titleMarginTop: "0em",
-          titleMarginBottom: "0em"
-        }
-      }
-    }} >
-      <Title level={2} ellipsis={ellipsisConfig} style={{ color: "Black" }}>{titleText}</Title>
-    </ConfigProvider>
+    <h2 style={{
+      color: "Black",
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+      whiteSpace: "nowrap",
+      fontFamily: "font-title"
+    }}>{titleText}</h2>
   )
 }
-let APrize = [
-  { children: prize[0] },
-  { children: prize[1] },
-  { children: prize[2] }
-]
 
 let albumData = [
   { key: "1", label: "厂牌", children: "SME" },
@@ -130,7 +124,7 @@ let albumData = [
   { key: "4", label: "发售时间", children: "" },
   { key: "5", label: "类型", children: "" },
   { key: "6", label: "风格", children: "" },
-  { key: "7", label: "价格", children: <Timeline pending="now prize" reverse items={APrize} /> },
+  { key: "7", label: "价格", children: <Timeline pending="now prize" reverse items={albumInfo.prize} /> },
   { key: "8", label: " ", children: "" },
   { key: "9", label: "持有&想要数", children: "" },
 ]
