58f9e40887d0486b3904ea01f001ff17fb9fc709
= <=>
2024-04-16 17:24:50 +0900

    update

--- flask_server.py
+++ flask_server.py
@@ -18,16 +18,30 @@ def getById(id):
         info['jacket_url'] = d['thumb']
         info['have'] = d['community']['have']
         info['want'] = d['community']['want']
-        info['country'] = d['country']
+        try:
+            info['country'] = d['country']
+        except:
+            info['country'] = ''
         info['format'] = d['formats'][0]['qty'] + ' x ' + d['formats'][0]['name']
         for s in d['formats'][0]['descriptions']:
             info['format'] += ', ' + s
         info['genre'] = ' '.join(d['genres'])
-        info['style'] = ' '.join(d['styles'])
-        info['label'] = d['labels'][0]['name'] + ' - ' + d['labels'][0]['catno']
-        info['low'] = d['lowest_price']
-        info['released'] = d['released_formatted']
-        
+        try:
+            info['style'] = ' '.join(d['styles'])
+        except:
+            info['style'] = ''
+        try:
+            info['label'] = d['labels'][0]['name'] + ' - ' + d['labels'][0]['catno']
+        except:
+            info['label'] = ''
+        try:
+            info['low'] = d['lowest_price']
+        except:
+            info['low'] = '0'
+        try:
+            info['released'] = d['released_formatted']
+        except:
+            info['released'] = ''
         try:
             info['sells'] = '{0}￥起{1}件在售'.format(str(round(int(d['lowest_price']))), d['num_for_sale'])
         except:
@@ -45,9 +59,13 @@ def getById(id):
         info['F'] = str(round(int(dd['Fair (F)']['value'])))
         info['P'] = str(round(int(dd['Poor (P)']['value'])))
         
-        for t in d['tracklist']:
-            info['pos'].append(t['position'])
-            info['title'].append((t['title']))
+        try:
+            for t in d['tracklist']:
+                info['pos'].append(t['position'])
+                info['title'].append((t['title']))
+        except:
+            info['pos'] = []
+            info['title'] = []
     return info
 
 @app.route("/")
